aded6cdb77d2e8843b583bb3e66987d7d46dcb35
Mr. Joseph <fullstackjake@gmail.com>
2018-08-16 10:02:49 -0400

    Building email funtion - nex is email reading view

--- src/Components/Body/Emails.js
+++ src/Components/Body/Emails.js
@@ -6,12 +6,41 @@ import Email from './Email'
 const emailData = emails.messages
 
 class Emails extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { readingEmail: true }
+    this.closeEmail = this.closeEmail.bind(this)
+  }
+
+  closeEmail() {
+    console.log('Clicked Close')
+    const readingEmail = this.state.readingEmail
+    if (readingEmail) {
+      this.setState({ readingEmail: false })
+    }
+
+    // console.log('this');
+  }
+
   render() {
+    const readingEmail = this.state.readingEmail
+    let emailView
+
+    if (readingEmail === false) {
+      emailView = emailData.map((email, index) => (
+        <Email key={index} email={email} />
+      ))
+    } else {
+      emailView = (
+        <h1>
+          I am reading and email{' '}
+          <button onClick={this.closeEmail}>Close</button>
+        </h1>
+      )
+    }
     return (
       <div className="Emails">
-        <div>
-          {emailData.map((email, index) => <Email key={index} email={email} />)}
-        </div>
+        <div>{emailView}</div>
       </div>
     )
   }
